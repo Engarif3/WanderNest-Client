af8afd9a6625b293a4a13e4f48d4cf61110b8c6f
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-07-13 17:56:30 +0200

    website title has been changed

--- src/components/shared/Navbar/Logo.jsx
+++ src/components/shared/Navbar/Logo.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import logo from "../../../assets/images/logo.png"
-import { Link } from 'react-router-dom';
+import logo from "../../../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
-    return (
-        <Link>
-        <div className='flex'>
-            <img className='hidden md:block animate-spin' src={logo} alt="logo" width="30" />
-            <p  className='text-xl p-0 italic pl-2 text-orange-600 font-extrabold'>WanderNest</p>
-        </div>
-        </Link>
-    );
+  return (
+    <Link>
+      <div className="flex">
+        <img
+          className="hidden md:block animate-spin"
+          src={logo}
+          alt="logo"
+          width="30"
+        />
+        <p className="text-xl p-0 italic pl-2 text-orange-600 font-extrabold">
+          WanderNest
+        </p>
+      </div>
+    </Link>
+  );
 };
 
 export default Logo;
