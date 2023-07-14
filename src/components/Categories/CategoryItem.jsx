4d542b025ba21ae8ab68038ec972c0b127888196
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-07-14 16:06:57 +0200

    css added to categoryItem component to solve the problem of hover:animate-bounce in tailwind css

--- src/components/Categories/CategoryItem.jsx
+++ src/components/Categories/CategoryItem.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import "./CategoryItem.css"
 
 const CategoryItem = ({ category }) => {
   const { label, icon: Icon } = category;
   return (
     <div className="w-28  border-b-4  hover:bg-red-600 hover:text-white rounded-md border-red-600 text-neutral-500 py-2 mx-4 mb-2 cursor-pointer">
-      <div className="flex flex-col justify-center items-center hover:animate-bounce">
+      <div className="flex flex-col justify-center items-center hover:bounceNew">
         <div className="text-sky-900">
         <Icon size={26} />
         </div>
